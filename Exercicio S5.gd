21c11ceb96f6ffec8b2478c0a93508ab265c563a
t-reitz <thomas.reitzfeld@sou.inteli.edu.br>
2023-03-03 22:19:42 -0300

    FINAL
    AMNSD FKSJNF

--- Exercicio S5.gd
+++ Exercicio S5.gd
@@ -1,18 +1,17 @@
 extends Node2D
 
-var teste = false
-var valor = 0
 var numero = 0 #Correcao ú por u
 var lista = [] #Correcao adicionado 'var' antes de lista
 var nome #Adicao de declaracao da variavel nome
+var cont = 0 # Correcao adicionado 'var' antes de cont
 
 func _on_Button_pressed():
 	#Coletando dados inseridos pelo usuário
-	numero = int($LineEdit2.text) 
+	numero = int($LineEdit.text) 
 	# Correcao ú por u 
 	# Adicao de '$' antes de LineEdit
 	# Criacao de outro lineEdit para o numero
-	nome = $LineEdit.text #Correcao trocar numero e $LineEdit.text de ordem
+	nome = $LineEdit2.text #Correcao trocar numero e $LineEdit.text de ordem
 
 func _on_Button2_pressed():
 	# Incrementando o número inserido pelo usuário
@@ -24,11 +23,22 @@ func _on_Button2_pressed():
 func _on_Button3_pressed():
 	#Mudando o nome do usuário de acordo com os dados da lista
 	#Se houver algum número ímpar o nome deve adicionar "baldo" ao final
-	while(len(lista)):
-		var cont = 0 # Correcao adicionado 'var' antes de cont
-		var i = 0 # Correcao adicionado 'var' antes de i
-		if(lista[i]%2==1):
-			cont+=1
-		if(cont!=0):
-			nome = nome+"baldo"
-		$Label2.text = nome
+	
+	#Check se tem numero impar na lista
+	for i in range(len(lista)):
+		if(lista[i] % 2 == 1):
+			cont += 1	
+	if(cont != 0):
+		nome = nome+"baldo"
+	#Set labels com lista e nome atualizado	
+	$Label2.text = nome
+	$Label6.text = String(lista)
+
+
+
+
+
+
+
+
+
